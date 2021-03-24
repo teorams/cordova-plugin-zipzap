7d42b1fed95eb7305e37d24036e370f59a218983
teo <teo@pop-os.localdomain>
2021-03-24 19:53:09 +0800

    first commit

---
+++ src/android/ZipZap.java
@@ -0,0 +1,61 @@
+package org.trg.zipzap;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import net.lingala.zip4j.core.ZipFile;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class ZipZap extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        try {
+            if (action.equals("unzip")) {
+                unzip(args.getString(0), args.getString(1), args.getString(2));
+            }
+
+            callbackContext.success(1);
+            return true;
+        } catch (Exception e) {
+            callbackContext.error(e.getMessage());
+            return false;
+        }
+    }
+
+    private void unzip(String from, String to, String password) throws Exception {
+        try {
+
+            String fromPath = removePathProtocolPrefix(from);
+            String toPath = removePathProtocolPrefix(to);
+
+            ZipFile zipFile = new ZipFile(fromPath);
+            if (!password.isEmpty() && zipFile.isEncrypted()) {
+                zipFile.setPassword(password);
+            }
+
+            zipFile.extractAll(toPath);
+
+        } catch (Exception e) {
+            e.getMessage();
+        }
+    }
+
+    private static String removePathProtocolPrefix(String path) {
+
+        if (path.startsWith("file://")) {
+            path = path.substring(7);
+        } else if (path.startsWith("file:")) {
+            path = path.substring(5);
+        }
+
+        return path.replaceAll("//", "/");
+
+    }
+}
---
+++ www/ZipZap.js
@@ -0,0 +1,49 @@
+var exec = require('cordova/exec');
+
+function isNullOrWhitespace(text) {
+    return !text || !text.replace(/\s/g, '').length;
+}
+
+function validateConfig(config, mode, callback) {
+    if (typeof (config) !== 'object') {
+        return callback('config is required and must be an object');
+    }
+    if (typeof(config.from) !== 'string' || isNullOrWhitespace(config.from)) {
+        return callback('config.from is required and must be a string');
+    }
+    if (typeof(config.to) !== 'string' || isNullOrWhitespace(config.to)) {
+        return callback('config.to is required and must be a string');
+    }
+    if (typeof(config.password) !== 'string' || isNullOrWhitespace(config.password)) {
+        config.password = '';
+    }
+
+    if (mode === 'unzip') {
+        window.resolveLocalFileSystemURL(config.from, function (result) {
+            if (!result.isFile) {
+                return callback('config.from must be a file url');
+            }
+            callback();
+        }, function (error) {
+            callback('failed while locating : ' + config.from + " (" + JSON.stringify(error) + ")");
+        });
+    }
+}
+
+
+exports.unzip = function (config, successCallback, errorCallback) {
+    setTimeout(function () {
+        try {
+            validateConfig(config, 'unzip', function (error) {
+                if (error) {
+                    return errorCallback(error);
+                }
+                exec(successCallback, errorCallback, 'ZipZap', 'unzip', [config.from, config.to, config.password]);
+            });
+        } catch (error) {
+            errorCallback(error.message);
+        }
+    });
+};
+
+
